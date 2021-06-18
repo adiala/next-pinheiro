6fcb405f73a4b1d256169ca7cf12e7b97a531542
adiala <david.adiala@gmail.com>
2021-06-18 16:39:09 -0300

    Cards Finished

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { Navbar } from '../components/Navbar';
 import { Hero } from '../components/Hero';
+import { Cards } from '../components/Cards';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
       <Navbar />
       <Hero />
+      <Cards />
     </div>
   );
 }
